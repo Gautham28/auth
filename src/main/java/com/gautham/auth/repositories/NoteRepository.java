8bc02e83b426effab6553b6e8e359ecddbf2523f
Your Name <gauthamk2003@gmail.com>
2026-03-29 16:41:24 +0530

    added Note repository

---
+++ src/main/java/com/gautham/auth/repositories/NoteRepository.java
@@ -0,0 +1,13 @@
+package com.gautham.auth.repositories;
+
+import com.gautham.auth.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository; // Imports the built-in Spring Data methods
+import java.util.List;
+
+// JpaRepository requires two things: <The Entity Type, The Data Type of the Primary Key>
+public interface NoteRepository<Note> extends JpaRepository<Note, Long> {
+
+    // We create a custom method here. Spring Data JPA is smart enough to see "findByOwnerUsername"
+    // and automatically write a SQL query like: SELECT * FROM notes WHERE owner_username = ?
+    List<Note> findByOwnerUsername(String ownerUsername);
+}
